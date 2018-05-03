551e465baa67536f15d8a743531799d753df6fb0
Josh Grams <josh@qualdan.com>
2018-05-03 17:27:45 -0400

    Add a second level introducing turrets.

--- levels.lua
+++ levels.lua
@@ -8,25 +8,40 @@ return {
 			branch = 0
 		},
 		contents = {
-			{ 'upgrade', 'down', min=1, max=1 },
+			{ 'upgrade', 'down', n=1 },
 			{ 'upgrade', 'money', min=2, max=5 },
 			{ 'jelly', n=1 }
 		},
 		background = {
 			{0.06, 0.06, 0.06}, {0.07, 0.06, 0.05}, {0.04, 0.04, 0.04}
 		}
-
+	},
+	{
+		tiles = 1800,
+		chances = {
+			directions = { 6, 5, 4, 0, 0, 0 },
+			rooms = { hex5 = 5, hex3 = 3 },
+			branch = 0.02
+		},
+		contents = {
+			{ 'upgrade', 'down', n=1 },
+			{ 'upgrade', 'money', min=4, max=7 },
+			{ 'upgrade', 'crystal', n=1 },
+			{ 'turret', 15, n=2 }
+		},
+		background = {
+			{0.06, 0.06, 0.06}, {0.07, 0.06, 0.05}, {0.04, 0.04, 0.04}
+		}
 	},
 	{
 		tiles = 1200,
-		origin = { x=0, y=0 },
 		chances = {
 			directions = { 8, 5, 4, 0, 3, 3 },
 			rooms = { rhomb=2, hex2=7, hex3=7 },
 			branch = 0.007
 		},
 		contents = {
-			{ 'upgrade', 'down', min=1, max=1 },
+			{ 'upgrade', 'down', n=1 },
 			{ 'upgrade', 'money', min=3, max=6 },
 			{ 'upgrade', 'boost', min=1, max=3 },
 			{ 'upgrade', 'bounce', n=0.5 },
@@ -49,7 +64,7 @@ return {
 			{ 'upgrade', 'money', min=2, max=7 },
 			{ 'upgrade', 'boost', min=1, max=3 },
 			{ 'upgrade', 'crystal', min=0, max=1 },
-			{ 'upgrade', 'reflect', min=1, max=1 },
+			{ 'upgrade', 'reflect', n=1 },
 			{ 'turret', min=3, max=5 },
 			{ 'jelly', min=4, max=7 },
 		},
